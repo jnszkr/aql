3d089059eea8d4f74a6daa9bb034c30bf37887a5
Oszkar Jona <oszkar.jona@netent.com>
2016-02-19 12:39:35 +0100

    testing

--- test/test.js
+++ test/test.js
@@ -1,16 +1,34 @@
+"use strict";
+
 var assert = require('assert'),
     aql = require('../aql');
 
-describe('items', function() {
-  describe('#find()', function () {
+describe('items', function () {
+    describe('#find()', function () {
 
-    it('should return "items.find().limit(100)" when criteria is not present', function () {
-      assert.equal(aql.items.find().query, 'items.find().limit(100)');
+        it('should return "items.find().limit(100)" when criteria is not present', function () {
+            assert.equal(aql.items.find().query, 'items.find().limit(100)');
+        });
+
+        it('should present criterias when it is set in parameter', function () {
+            assert.equal(aql.items.find({
+                repo: "abc"
+            }).query, 'items.find({"repo":"abc"}).limit(100)');
+        });
+
     });
 
-    it('should present criterias when it is set in parameter', function () {
-      assert.equal(aql.items.find({repo: "abc"}).query, 'items.find({"repo":"abc"}).limit(100)');
-    });
+    describe('#query()', () => {
 
-  });
+        it('should throw an error when input parameter is not DomainQuery object', () => {
+            let err = new Error("AQL: query parameter is invalid!");
+
+            assert.deepEqual(aql.query(), err);
+            assert.deepEqual(aql.query({}), err);
+            assert.deepEqual(aql.query(true), err);
+            assert.deepEqual(aql.query([]), err);
+            assert.deepEqual(aql.query("dafs"), err);
+        });
+
+    });
 });
