0276100e446fc0a8e0b078cd0c0437f159bc79e1
jnszkr <joszkar@gmail.com>
2019-04-12 11:08:38 +0200

    format code

--- aql.js
+++ aql.js
@@ -1,201 +1,207 @@
 "use strict";
 
-let rp = require('request-promise');
+let rp = require("request-promise");
 
 /**
  * Artifactory Query Language domain query.
  */
 class DomainQuery {
+  constructor(domain) {
+    this.domain = domain;
+    this.config = {
+      fields: []
+    };
 
-    constructor(domain) {
-        this.domain = domain;
-        this.config = {
-            fields: []
-        };
+    this.criteria = undefined;
+    this.fields = [];
+    this.limitConfig = undefined;
+    this.sortConfig = undefined;
+  }
 
-        this.criteria = undefined;
-        this.fields = [];
-        this.limitConfig = undefined;
-        this.sortConfig = undefined;
+  /**
+   * AQL query string.
+   *
+   * @returns {String}
+   */
+  get query() {
+    let conf = this.config,
+      _fields =
+        conf.fields.length > 0
+          ? `.include(${conf.fields.map(JSON.stringify).join(", ")})`
+          : "",
+      _criteria = conf.criteria ? JSON.stringify(conf.criteria) : "",
+      _sort = conf.sort ? `.sort(${JSON.stringify(conf.sort)})` : "",
+      _limit = conf.limit ? `.limit(${JSON.stringify(conf.limit)})` : "",
+      _offset =
+        conf.offset > 0 ? `.offset(${JSON.stringify(conf.offset)})` : "";
+
+    return `${
+      this.domain
+    }.find(${_criteria})${_fields}${_sort}${_offset}${_limit}`;
+  }
+
+  /**
+   * Find domain query with criteria.
+   *
+   * @param criteria
+   * @returns {DomainQuery}
+   */
+  find(criteria) {
+    criteria && (this.config.criteria = Object.assign({}, criteria));
+    return this;
+  }
+
+  /**
+   * Include fields.
+   *
+   *      domain.include("name", "repo")
+   *
+   * @returns {DomainQuery}
+   */
+  include() {
+    var args =
+        arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments),
+      schema = ["string"];
+
+    if (this._isValid(args, schema)) {
+      this.config.fields = this.config.fields.concat(args);
+    } else {
+      this._warn("Field configuration is not valid", args, schema);
     }
 
-    /**
-     * AQL query string.
-     *
-     * @returns {String}
-     */
-    get query() {
-        let conf = this.config,
-            _fields = conf.fields.length > 0 ? `.include(${conf.fields.map(JSON.stringify).join(', ')})` : '',
-            _criteria = conf.criteria ? JSON.stringify(conf.criteria) : '',
-            _sort = conf.sort ? `.sort(${JSON.stringify(conf.sort)})` : '',
-            _limit = conf.limit ? `.limit(${JSON.stringify(conf.limit)})` : '',
-            _offset = conf.offset > 0 ? `.offset(${JSON.stringify(conf.offset)})` : '';
+    return this;
+  }
 
+  /**
+   * Sort object
+   * @param sort
+   * @returns {DomainQuery}
+   */
+  sort(sort) {
+    let schema = {
+      $asc: ["string"],
+      $desc: ["string"]
+    };
 
-        return `${this.domain}.find(${_criteria})${_fields}${_sort}${_offset}${_limit}`;
+    if (this._isValid(sort, schema)) {
+      this.config.sort = sort;
+    } else {
+      this._warn("Sort configuration is not valid", sort, schema);
     }
 
-    /**
-     * Find domain query with criteria.
-     *
-     * @param criteria
-     * @returns {DomainQuery}
-     */
-    find(criteria) {
-        criteria && (this.config.criteria = Object.assign({}, criteria));
-        return this;
+    return this;
+  }
+
+  /**
+   * Limit
+   * @param num
+   * @returns {DomainQuery}
+   */
+  limit(num) {
+    let schema = 0;
+
+    if (this._isValid(num, schema)) {
+      this.config.limit = num;
+    } else {
+      this._warn("Limit configuration is not valid", num, schema);
     }
 
-    /**
-     * Include fields.
-     *
-     *      domain.include("name", "repo")
-     *
-     * @returns {DomainQuery}
-     */
-    include() {
-        var args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments)),
-            schema = ['string'];
+    return this;
+  }
 
-        if (this._isValid(args, schema)) {
-            this.config.fields = this.config.fields.concat(args);
-        } else {
-            this._warn('Field configuration is not valid', args, schema);
-        }
+  /**
+   * Offset
+   * @param num
+   * @returns {DomainQuery}
+   */
+  offset(num) {
+    let schema = 0;
 
-        return this;
+    if (this._isValid(num, schema)) {
+      this.config.offset = num;
+    } else {
+      this._warn("Offset configuration is not valid", num, schema);
     }
 
-    /**
-     * Sort object
-     * @param sort
-     * @returns {DomainQuery}
-     */
-    sort(sort) {
+    return this;
+  }
 
-        let schema = {
-            $asc: ['string'],
-            $desc: ['string']
-        };
+  /**
+   * Validate input objects with a schema.
+   *
+   * @param obj
+   * @param schema
+   * @returns {boolean}
+   * @private
+   */
+  _isValid(obj, schema) {
+    let typeofObj = Array.isArray(obj) ? "array" : typeof obj,
+      typeofSchema = Array.isArray(schema) ? "array" : typeof schema;
 
-        if (this._isValid(sort, schema)) {
-            this.config.sort = sort;
-        } else {
-            this._warn('Sort configuration is not valid', sort, schema);
-        }
-
-        return this;
+    if (typeofObj === typeofSchema) {
+      if (["string", "boolean", "number"].indexOf(typeofObj) > -1) {
+        return true;
+      } else if (typeofObj === "array" && schema.length > 0 && obj.length > 0) {
+        return obj.every(elem => this._isValid(elem, schema[0]));
+      } else if (typeofObj === "object") {
+        return Object.keys(obj).every(key => {
+          let isInSchema = Object.keys(schema).indexOf(key) > -1,
+            isValid = this._isValid(obj[key], schema[key]);
+          return isInSchema && isValid;
+        });
+      }
     }
 
-    /**
-     * Limit
-     * @param num
-     * @returns {DomainQuery}
-     */
-    limit(num) {
-        let schema = 0;
+    return false;
+  }
 
-        if (this._isValid(num, schema)) {
-            this.config.limit = num;
-        } else {
-            this._warn('Limit configuration is not valid', num, schema);
-        }
-
-        return this;
-    }
-
-    /**
-     * Offset
-     * @param num
-     * @returns {DomainQuery}
-     */
-    offset(num) {
-        let schema = 0;
-
-        if (this._isValid(num, schema)) {
-            this.config.offset = num;
-        } else {
-            this._warn('Offset configuration is not valid', num, schema);
-        }
-
-        return this;
-    }
-
-    /**
-     * Validate input objects with a schema.
-     *
-     * @param obj
-     * @param schema
-     * @returns {boolean}
-     * @private
-     */
-    _isValid(obj, schema) {
-        let typeofObj = Array.isArray(obj) ? 'array' : typeof obj,
-            typeofSchema = Array.isArray(schema) ? 'array' : typeof schema;
-
-        if (typeofObj === typeofSchema) {
-
-            if (['string', 'boolean', 'number'].indexOf(typeofObj) > -1) {
-                return true;
-            } else if (typeofObj === 'array' && schema.length > 0 && obj.length > 0) {
-                return obj.every(elem => this._isValid(elem, schema[0]));
-            } else if (typeofObj === 'object') {
-                return Object.keys(obj).every(key => {
-                    let isInSchema = Object.keys(schema).indexOf(key) > -1,
-                        isValid = this._isValid(obj[key], schema[key]);
-                    return isInSchema && isValid;
-                });
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Console output.
-     *
-     * @param message
-     * @param obj
-     * @param schema
-     * @private
-     */
-    _warn(message, obj, schema) {
-        console.warn(`AQL: ${message}: ${JSON.stringify(obj)}`);
-        console.warn(`     Provided object : ${JSON.stringify(obj)}`);
-        console.warn(`     Schema object   : ${JSON.stringify(schema)}`);
-    }
+  /**
+   * Console output.
+   *
+   * @param message
+   * @param obj
+   * @param schema
+   * @private
+   */
+  _warn(message, obj, schema) {
+    console.warn(`AQL: ${message}: ${JSON.stringify(obj)}`);
+    console.warn(`     Provided object : ${JSON.stringify(obj)}`);
+    console.warn(`     Schema object   : ${JSON.stringify(schema)}`);
+  }
 }
 
 module.exports = {
-    get items() {
-        return new DomainQuery('items');
-    },
-    get builds() {
-        return new DomainQuery('builds');
-    },
-    get archives() {
-        return new DomainQuery('archive.entries');
-    },
-    config: conf => {
-        let def = Object.assign({
-            method: 'POST',
-            transform: JSON.parse
-        }, conf);
-        this._request = rp.defaults(def);
-    },
-    /**
-     * Run query on your artifactory.
-     *
-     * @param {DomainQuery} query
-     * @returns {Promise}
-     */
-    query: query => {
-        if (query instanceof DomainQuery) {
-            query = query.query;
-        } else {
-            return Promise.reject("Invalid query parameter!");
-        }
-        return this._request({body: query}).promise();
+  get items() {
+    return new DomainQuery("items");
+  },
+  get builds() {
+    return new DomainQuery("builds");
+  },
+  get archives() {
+    return new DomainQuery("archive.entries");
+  },
+  config: conf => {
+    let def = Object.assign(
+      {
+        method: "POST",
+        transform: JSON.parse
+      },
+      conf
+    );
+    this._request = rp.defaults(def);
+  },
+  /**
+   * Run query on your artifactory.
+   *
+   * @param {DomainQuery} query
+   * @returns {Promise}
+   */
+  query: query => {
+    if (query instanceof DomainQuery) {
+      query = query.query;
+    } else {
+      return Promise.reject("Invalid query parameter!");
     }
-}
+    return this._request({ body: query }).promise();
+  }
+};
--- test/test.js
+++ test/test.js
@@ -1,41 +1,53 @@
 "use strict";
 
-var assert = require('assert'),
-    aql = require('../aql');
+var assert = require("assert"),
+  aql = require("../aql");
 
-describe('items', function () {
-    describe('#find()', function () {
-
-        it('should return empty find when criteria is not present', function () {
-            assert.equal(aql.items    .find().query, 'items.find()');
-            assert.equal(aql.builds   .find().query, 'builds.find()');
-            assert.equal(aql.archives .find().query, 'archive.entries.find()');
-        });
-
-        it('should present criterias when it is set in parameter', function () {
-            const criteria = { repo: "abc" }
-            assert.equal(aql.items    .find(criteria).query, 'items.find({"repo":"abc"})');
-            assert.equal(aql.builds   .find(criteria).query, 'builds.find({"repo":"abc"})');
-            assert.equal(aql.archives .find(criteria).query, 'archive.entries.find({"repo":"abc"})');
-        });
-
+describe("items", function() {
+  describe("#find()", function() {
+    it("should return empty find when criteria is not present", function() {
+      assert.equal(aql.items.find().query, "items.find()");
+      assert.equal(aql.builds.find().query, "builds.find()");
+      assert.equal(aql.archives.find().query, "archive.entries.find()");
     });
 
-    describe('#query()', () => {
-
-        it('should throw an error when input parameter is not DomainQuery object', () => {
-            let err = new Error("AQL: query parameter is invalid!");
-
-            // assert.deepEqual(aql.query(), err);
-            // assert.deepEqual(aql.query({}), err);
-            // assert.deepEqual(aql.query(true), err);
-            // assert.deepEqual(aql.query([]), err);
-            // assert.deepEqual(aql.query("dafs"), err);
-        });
-
-        it('should create a new DomainQuery instance for each query', () => {
-            assert.equal(aql.items    .find().include('name').query, 'items.find().include("name")');
-            assert.equal(aql.items    .find().include('repo').query, 'items.find().include("repo")');
-        });
+    it("should present criterias when it is set in parameter", function() {
+      const criteria = { repo: "abc" };
+      assert.equal(
+        aql.items.find(criteria).query,
+        'items.find({"repo":"abc"})'
+      );
+      assert.equal(
+        aql.builds.find(criteria).query,
+        'builds.find({"repo":"abc"})'
+      );
+      assert.equal(
+        aql.archives.find(criteria).query,
+        'archive.entries.find({"repo":"abc"})'
+      );
     });
+  });
+
+  describe("#query()", () => {
+    it("should throw an error when input parameter is not DomainQuery object", () => {
+      let err = new Error("AQL: query parameter is invalid!");
+
+      // assert.deepEqual(aql.query(), err);
+      // assert.deepEqual(aql.query({}), err);
+      // assert.deepEqual(aql.query(true), err);
+      // assert.deepEqual(aql.query([]), err);
+      // assert.deepEqual(aql.query("dafs"), err);
+    });
+
+    it("should create a new DomainQuery instance for each query", () => {
+      assert.equal(
+        aql.items.find().include("name").query,
+        'items.find().include("name")'
+      );
+      assert.equal(
+        aql.items.find().include("repo").query,
+        'items.find().include("repo")'
+      );
+    });
+  });
 });
